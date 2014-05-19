40c041e4e3a322f30afed1f3ac9b9c702fa54208
Billy Everyteen <aeckh002@fiu.edu>
2014-05-19 13:42:49 -0500

    Initial Commit

---
+++ Deprocrastinator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Deprocrastinator
+//
+//  Created by AE on 5/19/14.
+//  Copyright (c) 2014 Aaron Eckhart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
